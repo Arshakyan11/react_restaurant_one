a0f015a3db47ce9ddeb6327604159b747c0ca8dc
Arshakyan11 <erikarshakyan111@gmail.com>
2025-05-19 23:32:23 +0400

    project Setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import logo from "./logo.svg";
+import styles from ".//App.module.scss";
+function App() {
+  return (
+    <div >
+npm i react-router-dom
+    </div>
+  );
+}
+
+export default App;
