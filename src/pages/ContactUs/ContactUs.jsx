4a145f9adefa74993cfa97a96c8043050f0f6fb4
Arshakyan11 <erikarshakyan111@gmail.com>
2025-05-26 21:15:49 +0400

    minor change

--- src/pages/ContactUs/ContactUs.jsx
+++ src/pages/ContactUs/ContactUs.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "./ContactUs.module.scss";
+import { Field, Form, Formik } from "formik";
 const ContactUs = () => {
   return (
     <section className={styles.contactUsSec}>
@@ -10,6 +11,11 @@ const ContactUs = () => {
             We love hearing from our customers. Feel free to share your
             experience or ask any questions you may have.
           </p>
+          <Formik>
+            <Form>
+              <Field />
+            </Form>
+          </Formik>
         </div>
       </div>
     </section>
