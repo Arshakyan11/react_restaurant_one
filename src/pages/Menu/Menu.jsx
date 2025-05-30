e930963fb1589441e1488335d2fabf233cb26c22
Arshakyan11 <erikarshakyan111@gmail.com>
2025-05-31 01:39:40 +0400

    minor changes

--- src/pages/Menu/Menu.jsx
+++ src/pages/Menu/Menu.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
-import styles from "./Menu.module.scss"
-import Pagination from '../../components/Pagination/Pagination'
+import React from "react";
+import styles from "./Menu.module.scss";
+import Pagination from "../../components/Pagination/Pagination";
 const Menu = () => {
   return (
-    <div>
-    </div>
-  )
-}
+    <section className={styles.menuSec}>
+      <div className={styles.container}>
+        <div className={styles.restaurantMenu}>
+          <div className={styles.tutorialMenu}>
+            <span>Eriso</span>
+            <h2>
+              View Our <br /> New Menu
+            </h2>
+            <p>The freshest ingredients for you every day</p>
+          </div>
+          <h3 className={styles.menuTitle}>Menu</h3>
+          <div className={styles.filtersAndMeals}>
+            <div className={styles.filterBox}>
+              <h2>Filters</h2>
+              <div className={styles.filtersMain}>
+                 <ul className={styles.mealTime}>
+                    <li></li>
+                    <li></li>
+                    <li></li>
+                    <li></li>  
+                 </ul>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Menu
+export default Menu;
