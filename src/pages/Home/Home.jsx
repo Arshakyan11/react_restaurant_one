618a6d5f780aff3918ac2e97a3114d0fd0444cb0
Arshakyan11 <erikarshakyan111@gmail.com>
2025-07-11 15:43:15 +0400

    adding media

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -27,10 +27,19 @@ const Home = () => {
           <div className={styles.welcomeSec} data-aos="fade-up">
             <div className={styles.leftSide}>
               <img src={welcomeImg} alt="welcomeImg" />
+              <p className={styles.welcomeText}>
+                We’re thrilled to have you join us on a flavorful journey around
+                the world. Here, every dish is crafted with passion, every
+                ingredient is chosen with care, and every guest is treated like
+                family. Whether you're craving comforting classics or curious to
+                explore new tastes, we've prepared something special just for
+                you. Sit back, relax, and let your culinary adventure begin!
+              </p>
+              <Link to={ROUTES.MENU}>See our Menu</Link>
             </div>
             <div className={styles.rightSide}>
               <p className={styles.welcome}>
-                Welcome to <br /> <span>ERISO</span>
+                Welcome to <br className={styles.br} /> <span>ERISO</span>
               </p>
               <p className={styles.welcomeText}>
                 We’re thrilled to have you join us on a flavorful journey around
@@ -43,24 +52,36 @@ const Home = () => {
               <Link to={ROUTES.MENU}>See our Menu</Link>
             </div>
           </div>
-          <LittleMenuSection />
+          {/* <LittleMenuSection /> */}
           <div className={styles.reservationSec} data-aos="fade-up">
             <div className={styles.leftSideReserve}>
-              <img src={reserveImg1} alt="img" className={styles.mainImg} />
-              <img
-                src={reserveImg2}
-                alt="img"
-                className={styles.secondaryImg}
-              />
-              <img
-                src={reserveImg3}
-                alt="img"
-                className={styles.secondaryImg2}
-              />
+              <div className={styles.images}>
+                <img src={reserveImg1} alt="img" className={styles.mainImg} />
+                <img
+                  src={reserveImg2}
+                  alt="img"
+                  className={styles.secondaryImg}
+                />
+                <img
+                  src={reserveImg3}
+                  alt="img"
+                  className={styles.secondaryImg2}
+                />
+              </div>
+              <p>
+                Whether you're planning a romantic dinner, a family gathering,
+                or a special celebration, we’re here to make your experience
+                unforgettable. Reserve your table today and enjoy the finest
+                cuisine, warm hospitality, and a welcoming atmosphere. We
+                recommend booking in advance, especially on weekends, to ensure
+                the perfect spot for you and your guests. Simply select your
+                preferred date, time, and party size — and leave the rest to us.
+              </p>
+              <Link to={ROUTES.RESERVATION}>Reservation</Link>
             </div>
             <div className={styles.rightSideReserve}>
               <h2>
-                Let's reserve <br />
+                Let's reserve <br className={styles.br} />
                 <span>a table</span>
               </h2>
               <p>
